42188f190a8e344bdb5bda9c5b68aa9ce6eac68f
KDrip34 <124319502+KDrip34@users.noreply.github.com>
2023-02-07 16:07:18 -0800

    Create mealofmasculinemen.py

---
+++ mealofmasculinemen.py
@@ -0,0 +1,41 @@
+dinner_guests = ['andrewtate' , 'tristantate' , 'jwaller' , 'hamza' , 'sneako' , 'ceooftestorone' , 'davidgoggins' , 'jordanpetterson' , 'luke belmar']
+print(dinner_guests)
+Welcome_in = dinner_guests[3]
+print(' Welcome ' 'to the dinner you have been invited becuase you men have achieved to be a good influnce on the effeminated male world ' + Welcome_in.title() + '.')
+dinner_guests.remove('jordanpetterson')
+dinner_guests.insert(7, 'tykwandoe')
+Hello_again = dinner_guests[7]
+print(' Hello ' ' one of our dinner guests will not be making it sadly so I leave it up to you guys to vote on who is worthy enough and can come to the dinner next and it is ' + Hello_again.title() + '.') 
+dinner_guests.insert(0, 'fitxfearless')
+dinner_guests.insert(4, 'myron')
+dinner_guests.append('fresh')
+New_people = dinner_guests
+print(' Great ' '  fantastic news, the dinner table has drastically been increased becuase of the tate brothers gracsiously donating so we could have this beautiful masculine fellowship, we have some youtubers that preach about women nature that is joining us, welcome in '.join(dinner_guests))
+Apologizes_guests = dinner_guests.pop(4)
+print(' Apologizes ' ' last and final message due to the table not arrivng in time the dinner can only hold up to two guest at a time, week by week we will be brining in new guest and we will follow up with a youtbe video of fellow conversation while eating the dinner ' + Apologizes_guests.title() + '.')
+Apologizes_guests = dinner_guests.pop(0)
+print(' Apologizes ' ' last and final message due to the table not arrivng in time the dinner can only hold up to two guest at a time, week by week we will be brining in new guest and we will follow up with a youtbe video of fellow conversation while eating the dinner ' + Apologizes_guests.title() + '.')
+Apologizes_guests = dinner_guests.pop(5)
+print(' Apologizes ' ' last and final message due to the table not arrivng in time the dinner can only hold up to two guest at a time, week by week we will be brining in new guest and we will follow up with a youtbe video of fellow conversation while eating the dinner ' + Apologizes_guests.title() + '.')
+Apologizes_guests = dinner_guests.pop(6)
+print(' Apologizes ' ' last and final message due to the table not arrivng in time the dinner can only hold up to two guest at a time, week by week we will be brining in new guest and we will follow up with a youtbe video of fellow conversation while eating the dinner ' + Apologizes_guests.title() + '.')
+Apologizes_guests = dinner_guests.pop(7)
+print(' Apologizes ' ' last and final message due to the table not arrivng in time the dinner can only hold up to two guest at a time, week by week we will be brining in new guest and we will follow up with a youtbe video of fellow conversation while eating the dinner ' + Apologizes_guests.title() + '.')
+Apologizes_guests = dinner_guests.pop(3)
+print(' Apologizes ' ' last and final message due to the table not arrivng in time the dinner can only hold up to two guest at a time, week by week we will be brining in new guest and we will follow up with a youtbe video of fellow conversation while eating the dinner ' + Apologizes_guests.title() + '.')
+Apologizes_guests = dinner_guests.pop(2)
+print(' Apologizes ' ' last and final message due to the table not arrivng in time the dinner can only hold up to two guest at a time, week by week we will be brining in new guest and we will follow up with a youtbe video of fellow conversation while eating the dinner ' + Apologizes_guests.title() + '.')
+Apologizes_guests = dinner_guests.pop(2)
+print(' Apologizes ' ' last and final message due to the table not arrivng in time the dinner can only hold up to two guest at a time, week by week we will be brining in new guest and we will follow up with a youtbe video of fellow conversation while eating the dinner ' + Apologizes_guests.title() + '.')
+Apologizes_guests = dinner_guests.pop(3)
+print(' Apologizes ' ' last and final message due to the table not arrivng in time the dinner can only hold up to two guest at a time, week by week we will be brining in new guest and we will follow up with a youtbe video of fellow conversation while eating the dinner ' + Apologizes_guests.title() + '.')
+Apologizes_guests = dinner_guests.pop(2)
+print(' Apologizes ' ' last and final message due to the table not arrivng in time the dinner can only hold up to two guest at a time, week by week we will be brining in new guest and we will follow up with a youtbe video of fellow conversation while eating the dinner ' + Apologizes_guests.title() + '.')
+final_guest = dinner_guests[0]
+print(' The final guests ' 'congratulations! yoiu guys will be our very first guest for this week you have changed so many young men and there point of view with life and what they do with it, we have this dinner and this live stream nad video to celebrate you and talk to you fan to man ' + final_guest.title() + '.')
+final_guest = dinner_guests[1]
+print(' The final guests ' 'congratulations! yoiu guys will be our very first guest for this week you have changed so many young men and there point of view with life and what they do with it, we have this dinner and this live stream nad video to celebrate you and talk to you fan to man ' + final_guest.title() + '.')
+del dinner_guests[0]
+del dinner_guests[0]
+print(dinner_guests)
+
